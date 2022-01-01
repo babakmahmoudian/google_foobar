bbe0c6bcfab9c40ad3ed5f55c7605e0113884eda
B. Mahmoudian <20880438+babakmahmoudian@users.noreply.github.com>
2022-01-01 06:37:39 +0330

    implemented a BFS-based solution for dont_get_volunteered

--- 02_dont_get_volunteered/dont_get_volunteered.py
+++ 02_dont_get_volunteered/dont_get_volunteered.py
@@ -1,2 +1,44 @@
+from Queue import Queue
+
+
 def solution(src, dest):
-    pass
+    def get_possible_squares(current_square):
+        all_moves = []
+
+        if current_square % 8 <= 6:
+            all_moves += [-15, 17]
+
+        if current_square % 8 <= 5:
+            all_moves += [-6, 10]
+
+        if current_square % 8 >= 2:
+            all_moves += [6, -10]
+
+        if current_square % 8 >= 1:
+            all_moves += [15, -17]
+
+        all_squares = list(map(
+            lambda n: current_square + n, all_moves
+        ))
+
+        return [square for square in all_squares if 0 <= square <= 63 and square not in visited]
+
+    explored = Queue(64)
+    explored.put(src)
+
+    visited = {src}
+
+    distances = dict.fromkeys(range(64), -1)
+    distances[src] = 0
+
+    while not explored.empty():
+        current_square = explored.get()
+        next_squares = get_possible_squares(current_square)
+
+        if current_square == dest:
+            return distances[current_square]
+
+        for square in next_squares:
+            visited.add(square)
+            explored.put(square)
+            distances[square] = distances[current_square] + 1
--- 02_dont_get_volunteered/test_dont_get_volunteered.py
+++ 02_dont_get_volunteered/test_dont_get_volunteered.py
@@ -19,3 +19,43 @@ def test_given_cast_2():
     actual = solution(src, dest)
 
     assert actual == expected
+
+
+def test_same_source_dest():
+    src = 1
+    dest = 1
+    expected = 0
+
+    actual = solution(src, dest)
+
+    assert actual == expected
+
+
+def test_going_backwards():
+    src = 42
+    dest = 25
+    expected = 1
+
+    actual = solution(src, dest)
+
+    assert actual == expected
+
+
+def test_start_to_end():
+    src = 0
+    dest = 63
+    expected = 6
+
+    actual = solution(src, dest)
+
+    assert actual == expected
+
+
+def test_end_to_start():
+    src = 63
+    dest = 0
+    expected = 6
+
+    actual = solution(src, dest)
+
+    assert actual == expected
