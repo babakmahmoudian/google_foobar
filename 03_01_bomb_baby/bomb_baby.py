4f481de3d6db5fee7e245f44710b95588b959d77
B. Mahmoudian <20880438+babakmahmoudian@users.noreply.github.com>
2022-01-13 06:07:45 +0330

    implemented the solution for bomb_baby

--- 03_01_bomb_baby/bomb_baby.py
+++ 03_01_bomb_baby/bomb_baby.py
@@ -1,2 +1,10 @@
-def solution(x, y):
-    pass
+def solution(m, f):
+    M, F = [int(m), int(f)]
+    m, f = [M, F] if M > F else [F, M]
+
+    cycles = 0
+    while m % f != 0:
+        cycles += (m // f)
+        m, f = f, m % f
+
+    return 'impossible' if f != 1 else str(cycles + m - 1)
--- 03_01_bomb_baby/test_bomb_baby.py
+++ 03_01_bomb_baby/test_bomb_baby.py
@@ -2,30 +2,107 @@ from bomb_baby import solution
 
 
 def test_given_case_1():
-    x = '2'
-    y = '1'
-    expected = 1
+    m = '2'
+    f = '1'
+    expected = '1'
 
-    actual = solution(x, y)
+    actual = solution(m, f)
 
     assert actual == expected
 
 
-def test_given_case_1():
-    x = '4'
-    y = '7'
-    expected = 4
+def test_given_case_2():
+    m = '4'
+    f = '7'
+    expected = '4'
 
-    actual = solution(x, y)
+    actual = solution(m, f)
 
     assert actual == expected
 
 
 def test_given_case_impossible():
-    x = '2'
-    y = '4'
+    m = '2'
+    f = '4'
     expected = 'impossible'
 
-    actual = solution(x, y)
+    actual = solution(m, f)
 
     assert actual == expected
+
+
+def test_possible_large():
+    m = str(5 * pow(10, 40) + 123)
+    f = str(5 * pow(10, 40) + 124)
+
+    actual = solution(m, f)
+
+    assert actual != 'impossible'
+
+
+def test_impossible_large():
+    m = str(5 * pow(10, 40))
+    f = m
+
+    actual = solution(m, f)
+
+    assert actual == 'impossible'
+
+
+def test_one_one():
+    m = '5000'
+    f = '1'
+    expected = '4999'
+
+    actual = solution(m, f)
+
+    assert actual == expected
+
+
+def test_both_one():
+    m = '1'
+    f = '1'
+    expected = '0'
+
+    actual = solution(m, f)
+
+    assert actual == expected
+
+
+def test_same():
+    m = '5000'
+    f = '5000'
+    expected = 'impossible'
+
+    actual = solution(m, f)
+
+    assert actual == expected
+
+
+def test_multiply():
+    m = '50'
+    f = '500'
+    expected = 'impossible'
+
+    actual = solution(m, f)
+
+    assert actual == expected
+
+
+def test_big_diff_impossible():
+    m = '2'
+    f = str(5 * pow(10, 40))
+    expected = 'impossible'
+
+    actual = solution(m, f)
+
+    assert actual == expected
+
+
+def test_big_diff_possible():
+    m = '9'
+    f = str(5 * pow(10, 40))
+
+    actual = solution(m, f)
+
+    assert actual != 'impossible'
