04d358e4c120bd713d518ff148a2a62077926dcf
Babak Mahmoudian <babak.mahmoudian@gmail.com>
2022-01-25 22:32:03 +0330

    solved the challenge 'the greatest staircase of them all'

--- 03_03_the_grandest_staircase_of_them_all/the_grandest_staircase_of_them_all.py
+++ 03_03_the_grandest_staircase_of_them_all/the_grandest_staircase_of_them_all.py
@@ -1,2 +1,22 @@
 def solution(n):
-    pass
+    def get_staircases(remained_bricks_count, previous_height, memo):
+        if (remained_bricks_count, previous_height) in memo:
+            return memo[(remained_bricks_count, previous_height)]
+        if remained_bricks_count == 0:
+            return 1
+        if remained_bricks_count < 0:
+            return 0
+
+        staircase_count = 0
+        for stair_height in range(1, previous_height):
+            count = get_staircases(
+                remained_bricks_count - stair_height,
+                stair_height,
+                memo
+            )
+            staircase_count += count
+
+        memo[(remained_bricks_count, previous_height)] = staircase_count
+        return staircase_count
+
+    return get_staircases(n, n, {})
