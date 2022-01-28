ab9f9f814099a296d258a5b09983a256c7d27733
Babak Mahmoudian <babak.mahmoudian@gmail.com>
2022-01-28 07:53:54 +0330

    solved the challenge '04_01 free the bunny workers'

--- 04_01_free_the_bunny_workers/free_the_bunny_workers.py
+++ 04_01_free_the_bunny_workers/free_the_bunny_workers.py
@@ -1,2 +1,27 @@
+def factorial(n):
+    return n * factorial(n - 1) if n > 1 else 1
+
+
+def combination(n, r):
+    return factorial(n) / (factorial(r) * factorial(n - r))
+
+
 def solution(num_buns, num_required):
-    pass
+    def add_keys(idx_list, start_index, depth, remaining_keys):
+        if depth == num_required - 1:
+            key = remaining_keys.pop(-1)
+            for i in range(len(dist)):
+                if i not in idx_list:
+                    dist[i].insert(0, key)
+            return
+
+        depth += 1
+        for i in range(start_index, len(dist)):
+            add_keys(idx_list[:] + [i], i + 1, depth, remaining_keys)
+
+    key_count = combination(num_buns, num_required - 1)
+
+    dist = [[] for i in range(num_buns)]
+    add_keys([], 0, 0, range(key_count))
+
+    return dist
